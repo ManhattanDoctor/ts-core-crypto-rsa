853fe8bca11c4c107e6e1a9e9b241cf7648337f5
Renat Gubaev <renat.gubaev@gmail.com>
2022-08-30 15:03:18 +0300

    no message

---
+++ src/transport/TransportCryptoManagerRSA.ts
@@ -0,0 +1,42 @@
+import * as _ from 'lodash';
+import { ISignature, ITransportCommand, ITransportCryptoManager } from '@ts-core/common';
+import { RSA } from '../RSA';
+
+export class TransportCryptoManagerRSA implements ITransportCryptoManager {
+    // --------------------------------------------------------------------------
+    //
+    //  Static Methods
+    //
+    // --------------------------------------------------------------------------
+
+    public static ALGORITHM = 'RSA';
+
+    // --------------------------------------------------------------------------
+    //
+    //  Public Methods
+    //
+    // --------------------------------------------------------------------------
+
+    public async sign<U>(command: ITransportCommand<U>, nonce: string, privateKey: string): Promise<string> {
+        return RSA.sign(this.toString(command, nonce), privateKey);
+    }
+
+    public async verify<U>(command: ITransportCommand<U>, signature: ISignature): Promise<boolean> {
+        return RSA.verify(this.toString(command, signature.nonce), signature.value, signature.publicKey);
+    }
+
+    // --------------------------------------------------------------------------
+    //
+    //  Protected Methods
+    //
+    // --------------------------------------------------------------------------
+
+    protected toString<U>(command: ITransportCommand<U>, nonce: string): string {
+        let request = !_.isNil(command.request) ? command.request.toString() : '';
+        return `${command.name}${request}${nonce}`;
+    }
+
+    public get algorithm(): string {
+        return TransportCryptoManagerRSA.ALGORITHM;
+    }
+}
